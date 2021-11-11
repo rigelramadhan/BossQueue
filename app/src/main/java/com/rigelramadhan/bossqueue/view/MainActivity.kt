8c09b02414dba44e4c9b3eec1c8496d547c67666
Rigel Ramadhan <rigelramadhan@gmail.com>
2021-11-11 11:09:35 +0700

    Init Commit

---
+++ app/src/main/java/com/rigelramadhan/bossqueue/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rigelramadhan.bossqueue.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rigelramadhan.bossqueue.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
