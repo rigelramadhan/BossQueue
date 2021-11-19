bb5bda5c6c5acc0f83fd0aa93063926c7aaac9a2
Nopal <naufalrm312@gmail.com>
2021-11-19 18:19:39 +0700

    tes4

--- app/src/main/java/com/rigelramadhan/bossqueue/view/MainActivity.kt
+++ app/src/main/java/com/rigelramadhan/bossqueue/view/MainActivity.kt
@@ -7,6 +7,6 @@ import com.rigelramadhan.bossqueue.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
     }
 }
