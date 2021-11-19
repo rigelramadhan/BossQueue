ec1fd967d66bf38cf0d029d1520f30d460162266
Nopal <naufalrm312@gmail.com>
2021-11-19 18:32:17 +0700

    tes4

---
+++ app/src/main/java/com/rigelramadhan/bossqueue/view/HistoryActivity.java
@@ -0,0 +1,17 @@
+package com.rigelramadhan.bossqueue.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rigelramadhan.bossqueue.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
---
+++ app/src/main/java/com/rigelramadhan/bossqueue/view/RegisterActivity3.java
@@ -0,0 +1,17 @@
+package com.rigelramadhan.bossqueue.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rigelramadhan.bossqueue.R;
+
+public class RegisterActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register3);
+    }
+}
