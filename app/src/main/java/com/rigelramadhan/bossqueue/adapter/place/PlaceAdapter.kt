9181289702b98351fb72ccf534347caad2ffafe4
Rigel Ramadhan <rigelramadhan@gmail.com>
2021-12-02 11:08:08 +0700

    Setting up recycler view adapter and deleting unused layouts

---
+++ app/src/main/java/com/rigelramadhan/bossqueue/adapter/place/PlaceAdapter.kt
@@ -0,0 +1,19 @@
+package com.rigelramadhan.bossqueue.adapter.place
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceAdapter() : RecyclerView.Adapter<PlaceViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/rigelramadhan/bossqueue/adapter/place/PlaceViewHolder.kt
@@ -0,0 +1,8 @@
+package com.rigelramadhan.bossqueue.adapter.place
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
